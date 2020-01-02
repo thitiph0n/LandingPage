ef4a160f93bd956aa94d14ef9187c57df02845d8
thitiphon <thitipontk@gmail.com>
2020-01-02 20:45:58 +0700

    firstComit

---
+++ js/main.js
@@ -0,0 +1,99 @@
+//DOM Elements
+const time = document.getElementById("time"),
+  greeting = document.getElementById("greeting"),
+  name = document.getElementById("name"),
+  focus = document.getElementById("focus");
+
+function showTime() {
+  let today = new Date(),
+    hour = today.getHours(),
+    min = today.getMinutes(),
+    sec = today.getSeconds();
+
+  //Set PM or AM
+  const amPm = hour >= 12 ? "PM" : "AM";
+
+  //12hr format
+  hour = hour % 12 || 12;
+
+  time.innerHTML = `${hour}<span>:</span>${addZero(min)}<span>:</span>${addZero(
+    sec
+  )} ${amPm}`;
+
+  setTimeout(showTime, 1000);
+}
+
+function addZero(n) {
+  return (parseInt(n, 10) < 10 ? "0" : "") + n;
+}
+
+function setBg() {
+  let today = new Date(),
+    hour = today.getHours();
+
+  if (hour < 12) {
+    document.body.style.backgroundImage = "url('../src/morning.jpg')";
+    greeting.textContent = "Good Morning";
+  } else if (hour < 18) {
+    document.body.style.backgroundImage = "url('../src/afternoon.jpg')";
+    greeting.textContent = "Good Afternoon";
+  } else {
+    document.body.style.backgroundImage = "url('../src/night.jpg')";
+    greeting.textContent = "Good Evening";
+    document.body.style.color = "white";
+  }
+}
+
+// Get Name
+function getName() {
+  if (localStorage.getItem("name") === null) {
+    name.textContent = "[Enter Name]";
+  } else {
+    name.textContent = localStorage.getItem("name");
+  }
+}
+
+// Set Name
+function setName(e) {
+  if (e.type === "keypress") {
+    // Make sure enter is pressed
+    if (e.which == 13 || e.keyCode == 13) {
+      localStorage.setItem("name", e.target.innerText);
+      name.blur();
+    }
+  } else {
+    localStorage.setItem("name", e.target.innerText);
+  }
+}
+
+// Get Focus
+function getFocus() {
+  if (localStorage.getItem("focus") === null) {
+    focus.textContent = "[Enter Focus]";
+  } else {
+    focus.textContent = localStorage.getItem("focus");
+  }
+}
+
+// Set Focus
+function setFocus(e) {
+  if (e.type === "keypress") {
+    // Make sure enter is pressed
+    if (e.which == 13 || e.keyCode == 13) {
+      localStorage.setItem("focus", e.target.innerText);
+      focus.blur();
+    }
+  } else {
+    localStorage.setItem("focus", e.target.innerText);
+  }
+}
+
+name.addEventListener("keypress", setName);
+name.addEventListener("blur", setName);
+focus.addEventListener("keypress", setFocus);
+focus.addEventListener("blur", setFocus);
+
+showTime();
+setBg();
+getName();
+getFocus();
